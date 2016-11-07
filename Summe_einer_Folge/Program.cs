4e6ef97e26cd3a1cbdefdc92367cbe90aedc6dcc
Jan-Marcus Eskes <jan-marcus@eskes.de>
2016-11-07 21:38:53 +0100

    Added Project Files

---
+++ Summe_einer_Folge/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Summe_einer_Folge
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int eingabe, ergebnis = 0;
+
+      Console.WriteLine("Bitte geben Sie eine Zahl ein");
+      int.TryParse(Console.ReadLine(), out eingabe);
+
+      for (int i = 1; i <= eingabe; i++)
+      {
+        ergebnis += i;
+        //Zusatzfeature
+        Console.WriteLine("{0} - ter Schleifendurchlauf, Summe bisher: {1}", i, ergebnis);
+      }
+      Console.WriteLine("Die Summe aller Zahlen von 1 bis {0} ist: {1}", eingabe, ergebnis);
+      Console.Read();
+    }
+  }
+}
